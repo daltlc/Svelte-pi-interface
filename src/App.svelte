0a4f9920dbd04edd27ec211c1bd3da3b277358b7
daltonlcarr <daltcarr@gmail.com>
2019-11-13 17:05:25 -0800

    initial

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Keypad from './Keypad.svelte';
+
+	let pin;
+	$: view = pin ? pin.replace(/\d(?!$)/g, '*') : 'enter your pin';
+
+	function handleSubmit() {
+		if(pin === 666){
+			console.log('welcome');
+		}
+
+	}
+</script>
+
+<h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
+
+<Keypad bind:value={pin} on:submit={handleSubmit}/>
