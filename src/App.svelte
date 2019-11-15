d96c7e20d40e644b36d69cc177730e30a2770851
daltonlcarr <daltcarr@gmail.com>
2019-11-14 20:37:07 -0800

    code

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,23 @@
 	import Keypad from './Keypad.svelte';
 
 	let pin;
-	let auth = false;
+	let user = {loggedIn: false};
 	$: view = pin ? pin.replace(/\d(?!$)/g, '*') : 'enter your pin';
 
-	function handleSubmit() {
-		// console.log(pin);
-		if(pin = 666)
-		{
-			auth = true;
-			console.log('Correct');
+	const handleSubmit = () => {
+		if(pin === '5555'){
+			user.loggedIn = true;
 		}
-
 	}
 </script>
+<style>
 
+</style>
+
+{#if !user.loggedIn}
 <h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
-
-<Keypad bind:value={pin} on:submit={handleSubmit}/>
+	<Keypad bind:value={pin} on:submit={handleSubmit}/>
+{/if}
+{#if user.loggedIn}
+<h1>Welcome</h1>
+{/if}
