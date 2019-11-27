7c841403e898b5dfbacd7f50f1935563b82197eb
daltonlcarr <daltcarr@gmail.com>
2019-11-26 19:59:06 -0800

    city temp and conditons displayed

--- src/Weather.svelte
+++ src/Weather.svelte
@@ -1,24 +1,56 @@
 <script>
 	import { onMount } from 'svelte';
+let name;
+let city;
+let kelvin;
+let celsius;
+let fahrenheit;
+let weatherConditions;
 
 onMount(async () => {
- let apiCall = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=5809844&APPID=e319fafbf44d2f659a1fb2c13277c106`)
+ let apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=5809844&APPID=e319fafbf44d2f659a1fb2c13277c106`)
 		.then(r => r.json())
         console.log(apiCall);
+        name = apiCall.name;
+        kelvin = apiCall.main.temp;
+        celsius = kelvin - 273;
+        fahrenheit = Math.floor(celsius * (9/5) + 32);
+        weatherConditions = apiCall.weather[0].description.toUpperCase();
+
 });
+
+
          
 </script>
 <style>
+  main {
+    position: relative;
+    max-width: 800px;
+    padding: 1em;
+  }
+
+  main :global(.meta) {
+    color: rgb(207, 207, 207);
+    font-size: 12px;
+    margin: 0 0 1em 0;
+  }
+
+  main :global(a) {
+    color: rgb(255, 255, 255);
+  }
   .weather_main {
     height: 40vh;
-    background-color: red;
+    background-color: rgb(24, 24, 24);
+    padding: 10px 20px;
+    color:white;
   }
 </style>
 
 <main>
   <div class="weather_main">
-  <!-- <h1>{city},{state}</h1>
-  <li>{temp}</li> -->
+  <h1>{name}</h1>
+  <h2>{fahrenheit}&#176</h2>
+  <h2>{weatherConditions}</h2>
   </div>
 </main>
 
